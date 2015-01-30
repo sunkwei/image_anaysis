2eff2537db658c6a39718101e3f24a6dda49df40
sunkw <sunkwei@gmail.com>
2015-01-30 12:06:50 +0800

    st2:使用不同颜色标示光流方向

--- st2/show.py
+++ st2/show.py
@@ -8,7 +8,7 @@
 #
 #################################################################
 
-import cv2, time
+import cv2, time, math
 import numpy as np
 
 FNAME = 'video/s.mp4'
@@ -18,7 +18,7 @@ cv2.namedWindow('origin')
 last = None
 opflow = None
 
-def draw_flow(img, flow, step=16):
+def draw_flow(img, flow, step=8):
     ''' 在img中画光流方向 '''
     h,w = img.shape[:2]
     y,x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1)
@@ -27,9 +27,17 @@ def draw_flow(img, flow, step=16):
     lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
     lines = np.int32(lines)
 
+    #            蓝:左      黄:下        红:右       绿:上
+    colors = [ (255,0,0), (0,255,255), (0,0,255), (0,255,0) ]
+
     for (x1,y1),(x2,y2) in lines:
-        if np.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) > 3:
-            cv2.line(img, (x1,y1), (x2,y2), (0,255,255), 1)
+        if np.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) > 2: # 仅仅显示较大距离的移动
+            # 根据方向决定颜色
+            ang = np.arctan2(-(y2-y1), x2-x1) # y轴反方向
+            ang += math.pi + math.pi/4
+            ang %= math.pi * 2
+            index = int(ang / (math.pi/2))
+            cv2.line(img, (x1,y1), (x2,y2), colors[index], 2)
             cv2.circle(img, (x1,y1), 1, (0, 255, 0), -1)
 
     return img 
@@ -53,7 +61,7 @@ while True:
     if opflow is not None:
         cv2.imshow('origin', draw_flow(img, opflow))
 
-    cv2.waitKey(10)
+    cv2.waitKey(1)
 
 
 
